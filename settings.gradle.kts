86302f9bd94d49f8e77367b31f4895ac98af8780
sryang <sarang628@gmail.com>
2025-11-25 10:06:35 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/TorangToml/libs.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "Main"        // 루트 프로젝트 정의
+include(":app")     // 하위 프로젝트 1
+include(":library") // 하위 프로젝트 2
+include(":library")
