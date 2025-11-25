3288383f56ff7c7f72d9847dd1db68c06dc973c5
sryang <sarang628@gmail.com>
2025-11-25 13:05:53 +0900

    update

---
+++ library/src/main/java/com/sarang/torang/MainDialogUiState.kt
@@ -0,0 +1,21 @@
+package com.sarang.torang
+
+data class MainDialogUiState(
+    val showShare: Boolean = false,
+    val showReport: Int? = null,
+    val deleteReview: Int? = null,
+    val showMenu: Int? = null,
+    val showComment: Int? = null,
+    val showRestaurant: Int? = null,
+    val mainDialogEvent: MainDialogEvent = MainDialogEvent()
+)
+
+data class MainDialogEvent(
+    val onCloseShare: () -> Unit = {},
+    val closeReport: () -> Unit = {},
+    val onDismissRequest: () -> Unit = {},
+    val onDelete: () -> Unit = {},
+    val onCloseMenu: () -> Unit = {},
+    val onReport: (Int) -> Unit = {},
+    val onDeleteMenu: (Int) -> Unit = {},
+)
